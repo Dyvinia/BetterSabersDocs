a0b907c8b4b62469787f4c2bd413c78463d1fcb6
Dyvine57 <dulana57@gmail.com>
2023-10-05 13:38:40 -0400

    caps check (maybe)

---
+++ docs/js/extra.js
@@ -0,0 +1,19 @@
+window.onload = () => {
+    currentURL = window.location.href;
+    lowerCaseURL = currentURL.toLowerCase();
+    if (currentURL != lowerCaseURL) {
+        if (!UrlExists(currentURL)){
+            location.replace(lowerCaseURL);
+        }
+    }
+};
+
+function UrlExists(url) {
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    if (http.status != 404)
+        return true;
+    else
+        return false;
+}
