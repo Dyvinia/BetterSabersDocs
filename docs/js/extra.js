776b491c6d7b0ff016070f1072b7ac57b3c8211a
Dyvine57 <dulana57@gmail.com>
2023-10-05 14:11:53 -0400

    optimize js

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -1,20 +1,15 @@
-window.onload = () => {
-    currentURL = window.location.href;
-    lowerCaseURL = currentURL.toLowerCase();
-    if (currentURL != lowerCaseURL) {
-        if (!UrlExists(currentURL)){
-            console.log("Redirecting from " + currentURL + " to " + lowerCaseURL);
-            location.replace(lowerCaseURL);
-        }
+currentURL = window.location.href;
+lowerCaseURL = currentURL.toLowerCase();
+if (currentURL != lowerCaseURL) {
+    if (!UrlExists(currentURL)){
+        console.log("Redirecting from " + currentURL + " to " + lowerCaseURL);
+        location.replace(lowerCaseURL);
     }
-};
+}
 
 function UrlExists(url) {
     var http = new XMLHttpRequest();
     http.open('HEAD', url, false);
     http.send();
-    if (http.status != 404)
-        return true;
-    else
-        return false;
+    return http.status != 404;
 }
